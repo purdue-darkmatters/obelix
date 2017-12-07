4755d1a1c3541c1388f4443776b76c20763ae374
Darryl <dmasson@purdue.edu>
2017-12-07 09:19:23 -0500

    Added versioning, updated README, added better CLI

--- src/obelix.cpp
+++ src/obelix.cpp
@@ -1,19 +1,58 @@
 #include "DAQ.h"
+#include "boost/program_options.hpp"
+
+namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        cout << "Usage: " << argv[0] << " config_file\n";
+    const string version("1.2.3");
+    const int default_buffer_size(1024);
+    int buffer_length(0);
+    po::options_description general_options("Allowed arguments");
+    general_options.add_options()
+        ("help,h", "produce this message")
+        ("config,c", po::value<string>(), "specify config file (requried)")
+        ("version,v", "output current version and return")
+    ;
+
+    po::options_description secret_options("Secret arguments");
+    secret_options.add_options()
+        ("buffer,b", po::value<int>()->default_value(default_buffer_size), "circular buffer size")
+    ;
+
+    po::options_description all_options("Allowed options");
+    all_options.add(general_options).add(secret_options);
+    po::variables_map vm;
+    try {
+        po::store(po::parse_command_line(argc, argv, all_options), vm);
+    } catch (exception& e) {
+        cout << e.what() << "\n";
+        cout << general_options << "\n";
         return 1;
     }
+
     cout << "Welcome to Obelix!\n";
-    string config_file = argv[1];
-    int buffer_length = argc > 2 ? atoi(argv[2]) : 1024;
+    if (vm.count("help")) {
+        cout << general_options << "\n";
+        return 1;
+    }
+    if (vm.count("version")) {
+        cout << "Version " << version << "\n";
+        return 1;
+    }
+    if (not vm.count("config")) {
+        cout << "Config file required\n";
+        return 1;
+    }
+    if (vm.count("buffer")) {
+        buffer_length = vm["buffer"].as<int>();
+    }
+    const string& config_file = vm["config"].as<string>();
     unique_ptr<DAQ> daq;
     try {
-    	daq = unique_ptr<DAQ>(new DAQ(buffer_length));
+        daq = unique_ptr<DAQ>(new DAQ(buffer_length));
     } catch (exception& e) {
-    	cout << "Why did this fail?" << e.what() << "\n";
-    	return 1;
+        cout << "Why did this fail?" << e.what() << "\n";
+        return 1;
     }
     try {
         daq->Setup(config_file);
